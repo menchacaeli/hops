3d2008869bed498a7d74d2abfe6d7060e50d7dbd
elimenchaca <menchacaeli@gmail.com>
2026-06-15 14:48:47 -0600

    feat: add data package barrel export

---
+++ src/data/index.ts
@@ -0,0 +1,7 @@
+// src/data/index.ts
+export * from './models';
+export * from './services/auth';
+export * from './services/beers';
+export * from './services/breweries';
+export * from './services/user';
+export * from './services/events';
